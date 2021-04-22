9fa46a0749357e1771b52d484d1da744ab347e9a
gh-actions <actions@github.com>
2021-04-22 04:14:45 +0000

    Deploy website - based on 902a057c101ad722e1e582db40656136f70628a7

---
+++ zh/assets/js/dcd7855b.d9443b4e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[222],{293:function(a){a.exports=JSON.parse('{"allTagsPath":"/zh/blog/tags","slug":"kubevela","name":"kubevela","count":2,"permalink":"/zh/blog/tags/kubevela"}')}}]);
