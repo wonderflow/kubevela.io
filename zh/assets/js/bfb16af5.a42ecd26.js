553941db8c8275141a02a50f8e3c84734c5afa7c
gh-actions <actions@github.com>
2021-04-30 03:21:19 +0000

    Deploy website - based on 06b376b5c368f385bde1ab874b1199c9248dfd98

--- zh/assets/js/bfb16af5.a42ecd26.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[198],{270:function(a){a.exports=JSON.parse('{"documentation":{"allTagsPath":"/zh/blog/tags","slug":"documentation","name":"documentation","count":1,"permalink":"/zh/blog/tags/documentation"},"kubevela":{"allTagsPath":"/zh/blog/tags","slug":"kubevela","name":"kubevela","count":2,"permalink":"/zh/blog/tags/kubevela"}}')}}]);
